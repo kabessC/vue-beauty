ab5c8fec498ae0596e0ec98f97acdece3e31a8d3
kabessC <chenjunkai1024@outlook.com>
2017-01-22 17:57:07 +0800

    Add badge component.

---
+++ vb/components/badge/badge.vue
@@ -0,0 +1,53 @@
+<template>
+  <span>
+    <slot></slot>
+    <transition>
+    </transition>
+    <span :class="ant-badge-status-text">{text}</span>
+  </span>
+</template>
+<script>
+export default {
+  name: 'vBadge',
+  data: () => ({
+    prefixCls: 'ant-badge',
+    status: '',
+  }),
+  props: {
+    count: {
+      type: Number,
+      default: 0
+    },
+    overflowCount: {
+      type: Number,
+      default: 99
+    },
+    dot: {
+      type: Boolean,
+      default: false
+    },
+    status: {
+      type: String,
+      default: '',
+      validator: function(value) {
+        return ['', 'success', 'processing', 'default', 'error', 'warning'].indexOf(value) > -1;
+      }
+    },
+    text: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    badgeCls: function() {
+      return [
+        this.prefixCls,
+        {
+          [`${this.prefixCls}-status`]: !!status,
+          [`${this.prefixCls}-not-a-wrapper`]: !!this.$slots.default
+        }
+      ];
+    }
+  }
+}
+</script>
